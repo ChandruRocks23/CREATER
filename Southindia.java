29dc497c0aa532cafe4c99a331857aa2ab29a522
Chandrasaravanan s <113888976+ChandruRocks23@users.noreply.github.com>
2022-10-30 17:24:14 +0530

    Add files via upload

---
+++ Southindia.java
@@ -0,0 +1,15 @@
+abstract class Southindia extends India{
+Southindia(){
+    super("Narendra Modi");
+}
+
+void cultivate(){
+    System.out.println("Rice and Wheat cultivation");
+}
+
+void livingStyle(){
+    System.out.println("Average Development");
+}
+
+
+}
