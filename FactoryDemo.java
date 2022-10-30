1c1831ca451993ccbe0b91d60b894cc724d6637c
Chandrasaravanan s <113888976+ChandruRocks23@users.noreply.github.com>
2022-10-30 17:23:16 +0530

    Add files via upload

---
+++ FactoryDemo.java
@@ -0,0 +1,27 @@
+abstract class FactoryDemo extends SmartPhone
+{
+
+boolean isOriginalPiece=false;
+static int price=0;
+
+abstract void verifyFingerPrint();
+
+abstract void providePattern();
+
+int call(int seconds){
+    
+    return seconds;
+}
+void sendMessage(){
+    System.out.println("Send the Message");
+}
+
+void receiveCall(){
+    System.out.println("Call Received");
+}
+
+void browse(){
+        System.out.println("FactoryDemo browsing");
+}
+
+}
---
+++ Samsung.java
@@ -0,0 +1,23 @@
+class Samsung extends FactoryDemo{
+
+static int price=5000;
+
+
+void verifyFingerPrint(){
+    System.out.println("Finger Print Varified");
+}
+
+void providePattern(){
+    System.out.println("Pattern Lock");
+}
+public static void main(String arg[]){
+    Samsung sam=new Samsung();
+    sam.browse();
+    System.out.println(sam.price);
+    sam.verifyFingerPrint();
+    sam.providePattern();
+    System.out.println(sam.call(10)+" seconds");
+    sam.sendMessage();
+    sam.receiveCall();
+}
+}
